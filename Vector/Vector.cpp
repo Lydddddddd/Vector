ec40e26af155a04c5e58883e45b02fb60e37daaa
ALW <ALW@ALIENWA>
2023-10-22 18:20:15 +0800

    添加项目文件。

---
+++ Vector/Vector.cpp
@@ -0,0 +1,93 @@
+// vector.cpp
+
+#include "vector.h"
+#include <stdexcept>
+
+//template <typename T>
+//Vector<T>::Vector(){}
+
+template <typename T>
+Vector<T>::Vector(size_t dim) : size(dim), data(dim) {}
+
+template <typename T>
+Vector<T>::~Vector() {}
+
+template <typename T>
+Vector<T>::Vector(const Vector<T>& other) {
+    size = other.size;
+    data = other.data;
+}
+
+template <typename T>
+Vector<T>& Vector<T>::operator=(const Vector<T>& other) {
+    if (this == &other) {
+        return *this;
+    }
+    size = other.size;
+    data = other.data;
+    return *this;
+}
+
+template <typename T>
+size_t Vector<T>::getDimension() const {
+    return size;
+}
+
+template <typename T>
+T& Vector<T>::operator[](size_t index) {
+    if (index >= size) {
+        throw std::out_of_range("Index out of range");
+    }
+    return data[index];
+}
+
+template <typename T>
+const T& Vector<T>::operator[](size_t index) const {
+    if (index >= size) {
+        throw std::out_of_range("Index out of range");
+    }
+    return data[index];
+}
+
+template <typename T>
+Vector<T> Vector<T>::operator+(const Vector<T>& other) const {
+    if (size != other.size) {
+        throw std::invalid_argument("Vector dimensions do not match");
+    }
+    Vector<T> result(size);
+    for (size_t i = 0; i < size; ++i) {
+        result[i] = data[i] + other[i];
+    }
+    return result;
+}
+
+template <typename T>
+bool Vector<T>::operator==(const Vector<T>& other) const {
+    return (size == other.size) && (data == other.data);
+}
+
+template <typename T>
+bool Vector<T>::operator!=(const Vector<T>& other) const {
+    return !(*this == other);
+}
+
+template <typename U>
+std::ostream& operator<<(std::ostream& out, const Vector<U>& vec) {
+    out << "(";
+    for (size_t i = 0; i < vec.size; ++i) {
+        out << vec[i];
+        if (i < vec.size - 1) {
+            out << ", ";
+        }
+    }
+    out << ")";
+    return out;
+}
+
+template <typename U>
+std::istream& operator>>(std::istream& in, Vector<U>& vec) {
+    for (size_t i = 0; i < vec.size; ++i) {
+        in >> vec.data[i];
+    }
+    return in;
+}
---
+++ Vector/Vector.h
@@ -0,0 +1,37 @@
+#pragma once
+// Vector.h
+
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+template <typename T>
+class Vector {
+private:
+    vector<T> data;
+    size_t size;
+
+public:
+    /*Vector();*/
+    Vector(size_t dim = 0);
+    ~Vector();
+
+    Vector(const Vector<T>& other);
+    Vector<T>& operator=(const Vector<T>& other);
+
+    size_t getDimension() const;
+
+    T& operator[](size_t index);
+    const T& operator[](size_t index) const;
+
+    Vector<T> operator+(const Vector<T>& other) const;
+    bool operator==(const Vector<T>& other) const;
+    bool operator!=(const Vector<T>& other) const;
+
+    template <typename U>
+    friend ostream& operator<<(ostream& out, const Vector<U>& vec);
+
+    template <typename U>
+    friend istream& operator>>(istream& in, Vector<U>& vec);
+};
